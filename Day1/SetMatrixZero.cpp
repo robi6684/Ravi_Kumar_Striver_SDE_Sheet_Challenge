1784351f0a8916a9ea64cff43893222f6b64c1f9
robi6684 <robi6684vd@gmail.com>
2022-06-05 16:03:46 +0530

    Create SetMatrixZero.cpp
    Day1 - 1 problem Solved

---
+++ Day1/SetMatrixZero.cpp
@@ -0,0 +1,69 @@
+void setZeroes(vector<vector<int>>& matrix) {
+    bool r=false, c=false;
+    for(int i=0; i<matrix.size(); i++)
+    {
+        if(matrix[i][0] == 0)
+        {
+            r = true;
+            break;
+        }
+    }
+    for(int i=0; i<matrix[0].size(); i++)
+    {
+        if(matrix[0][i] == 0)
+        {
+            c = true;
+            break;
+        }
+    }
+    
+    for(int i=1; i<matrix.size(); i++)
+    {
+        for(int j=1; j<matrix[i].size(); j++)
+        {
+            if(matrix[i][j] == 0)
+            {
+                matrix[i][0] = 0;
+                matrix[0][j] = 0;
+            }
+        }
+    }
+    
+    for(int i=1; i<matrix.size(); i++)
+    {
+        if(matrix[i][0] == 0)
+        {
+            for(int k=0; k<matrix[i].size(); k++)
+            {
+                matrix[i][k] = 0;
+            }
+        }
+    }
+    
+    for(int i=1; i<matrix[0].size(); i++)
+    {
+        if(matrix[0][i] == 0)
+        {
+            for(int k=0; k<matrix.size(); k++)
+            {
+                matrix[k][i] = 0;
+            }
+        }
+    }
+    
+    if(r == true)
+    {
+        for(int i=0; i<matrix.size(); i++)
+        {
+            matrix[i][0] = 0;
+        }
+    }
+    if(c == true)
+    {
+        for(int i=0; i<matrix[0].size(); i++)
+        {
+            matrix[0][i] = 0;
+        }
+    }
+        
+    }
